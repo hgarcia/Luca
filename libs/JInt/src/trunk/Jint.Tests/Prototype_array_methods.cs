5a2c2e01452e4c3b3015a079662cf47ce8ebb5c1
hgarcia <hernan@dynamicprogrammer.com>
2010-07-22 16:31:45 -0400

    Implemented, intersect, last, first, indexOf and flatten for Array

--- libs/JInt/src/trunk/Jint.Tests/Prototype_array_methods.cs
+++ libs/JInt/src/trunk/Jint.Tests/Prototype_array_methods.cs
@@ -4,12 +4,158 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Jint.Tests
 {
     [TestClass]
-    public class Prototype_array_methods
+    public class When_we_extend_arrays_with_PrototypeExtensions
     {
         public IList<IExtensionRegister> prototype = new List<IExtensionRegister> {new Jint.PrototypeExtension.Registration()};
+        
+        [TestMethod]
+        public void should_support_intersect()
+        {
+            var jint = new JintEngine(prototype);
+            dynamic result = jint.Run(@"var ar = ['john','ruiz','mary','bob','julian','esther',3];
+var br = ['john','mary','luther','bob','cesar',3,'esther',6,new Date()];
+return ar.intersect(br);
+");
+            Assert.AreEqual(5,result.Count);
+            Assert.AreEqual("john", result[0]);
+            Assert.AreEqual("mary", result[1]);
+            Assert.AreEqual("bob", result[2]);
+            Assert.AreEqual("esther", result[3]);
+            Assert.AreEqual(3, result[4]);
+
+
+        }
 
         [TestMethod]
-        public void Array_supports_clone()
+        public void if_value_to_search_is_of_the_wrong_type_indexOf_should_return_minus_one()
+        {
+            var jint = new JintEngine(prototype);
+            dynamic result = jint.Run(@"var ar = ['1','2','3'];
+return ar.indexOf(2);
+");
+            Assert.AreEqual(-1, result);            
+        }
+
+        [TestMethod]
+        public void if_value_to_search_is_not_found_indexOf_should_return_minus_one()
+        {
+            var jint = new JintEngine(prototype);
+            dynamic result = jint.Run(@"var ar = ['frank','bob','mary'];
+return ar.indexOf('joseph');
+");
+            Assert.AreEqual(-1, result);
+        }
+        [TestMethod]
+        public void if_value_to_search_is_not_passed_indexOf_should_return_minus_one()
+        {
+            var jint = new JintEngine(prototype);
+            dynamic result = jint.Run(@"var ar = ['frank','bob','mary'];
+return ar.indexOf();
+");
+            Assert.AreEqual(-1, result);
+        }
+
+        [TestMethod]
+        public void if_array_is_empty_indexOf_should_return_minus_one()
+        {
+            var jint = new JintEngine(prototype);
+            dynamic result = jint.Run(@"var ar = [];
+return ar.indexOf('bob');
+");
+            Assert.AreEqual(-1, result);
+        }
+
+        [TestMethod]
+        public void should_support_indexOf()
+        {
+            var jint = new JintEngine(prototype);
+            dynamic result = jint.Run(@"var ar = ['frank','bob','lisa'];
+return ar.indexOf('bob');
+");
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void should_support_flattern()
+        {
+            var jint = new JintEngine(prototype);
+            dynamic result = jint.Run(@"var a = ['frank', ['bob', 'lisa'], ['jill', ['tom', 'sally']]];
+var b = a.flatten();
+return [a,b];
+");
+            var a = result[0];
+            var b = result[1];
+            Assert.AreEqual("frank",a[0]);
+            Assert.AreEqual("bob", a[1][0]);
+            Assert.AreEqual("lisa", a[1][1]);
+            Assert.AreEqual("jill", a[2][0]);
+            Assert.AreEqual("tom", a[2][1][0]);
+            Assert.AreEqual("sally", a[2][1][1]);
+
+
+            Assert.AreEqual("frank",b[0]);
+            Assert.AreEqual("bob", b[1]);
+            Assert.AreEqual("lisa", b[2]);
+            Assert.AreEqual("jill", b[3]);
+            Assert.AreEqual("tom", b[4]);
+            Assert.AreEqual("sally", b[5]);
+        }
+
+        [TestMethod]
+        public void if_array_is_empty_last_should_return_null()
+        {
+            var jint = new JintEngine(prototype);
+            dynamic result = jint.Run(@"var ar = [];
+return ar.last();
+");
+            Assert.IsNull(result);
+        }
+        [TestMethod]
+        public void should_support_last()
+        {
+            var jint = new JintEngine(prototype);
+            dynamic result = jint.Run(@"var ar = [1,2,3,4];
+return ar.last();
+");
+            Assert.AreEqual(4,result);            
+        }
+
+        [TestMethod]
+        public void if_array_is_empty_first_should_return_null()
+        {
+            var jint = new JintEngine(prototype);
+            dynamic result = jint.Run(@"var ar = [];
+return ar.first();
+");
+            Assert.IsNull(result);
+        }
+        [TestMethod]
+        public void should_support_first()
+        {
+            var jint = new JintEngine(prototype);
+            dynamic result = jint.Run(@"var ar = [1,2,3,4];
+return ar.first();
+");
+            Assert.AreEqual(1,result);
+        }
+
+        [TestMethod]
+        public void should_support_compact()
+        {
+            var jint = new JintEngine(prototype);
+            dynamic result = jint.Run(@"var ar = [1,2,null,3, undefined,6, 'null'];
+return ar.compact()");
+
+            Assert.AreEqual(1,result[0]);
+            Assert.AreEqual(2, result[1]);
+            Assert.AreEqual(3, result[2]);
+            Assert.AreEqual(6, result[3]);
+            Assert.AreEqual("null", result[4]);
+
+        }
+
+        [TestMethod]
+        public void should_support_clone()
         {
             var jint = new JintEngine(prototype);
             dynamic result = jint.Run(@"var ar = [1,2,3,4];
@@ -24,7 +170,7 @@ namespace Jint.Tests
             Assert.AreEqual(3, result[2][0]);
         }
         [TestMethod]
-        public void Array_supports_clear()
+        public void should_supports_clear()
         {
             var jint = new JintEngine(prototype);
             dynamic result = jint.Run(@"var ar = [1,2,3,4];
@@ -35,7 +181,7 @@ namespace Jint.Tests
         }
 
         [TestMethod]
-        public void Array_should_support_collect_method()
+        public void should_support_collect()
         {
             var jint = new JintEngine(prototype);
             dynamic result = jint.Run(@"var ar = [1,2,3,4];
@@ -48,7 +194,7 @@ return ar.collect(function(a){
             Assert.AreEqual(5, result[3]);
         }
         [TestMethod]
-        public void Array_collect_method_if_func_is_not_passed_returns_same_array()
+        public void if_collect_dont_get_a_func_should_return_same_array()
         {
             var jint = new JintEngine(prototype);
             dynamic result = jint.Run(@"var ar = [1,2,3,4];
@@ -59,7 +205,7 @@ return ar.collect();");
             Assert.AreEqual(4, result[3]);
         }
         [TestMethod]
-        public void Array_collect_method_if_null_is_passed_returns_same_array()
+        public void if_collect_gets_a_null_func_should_return_same_array()
         {
             var jint = new JintEngine(prototype);
             dynamic result = jint.Run(@"var ar = [1,2,3,4];
@@ -70,7 +216,7 @@ return ar.collect(null);");
             Assert.AreEqual(4, result[3]);
         }
         [TestMethod]
-        public void Array_collect_method_if_null_string_is_passed_returns_same_array()
+        public void if_collect_gets_another_object_instead_of_a_func_should_return_same_array()
         {
             var jint = new JintEngine(prototype);
             dynamic result = jint.Run(@"var ar = [1,2,3,4];
--- libs/JInt/src/trunk/Jint/PrototypeExtension/PrototypeArrays.cs
+++ libs/JInt/src/trunk/Jint/PrototypeExtension/PrototypeArrays.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using Jint.Native;
 
 namespace Jint.PrototypeExtension
@@ -12,6 +14,94 @@ namespace Jint.PrototypeExtension
             Target.Prototype.DefineOwnProperty("collect", Target.Global.FunctionClass.New<JsObject>(CollectImpl), PropertyAttributes.DontEnum);
             Target.Prototype.DefineOwnProperty("clear", Target.Global.FunctionClass.New<JsArray>(ClearImpl), PropertyAttributes.DontEnum);
             Target.Prototype.DefineOwnProperty("clone", Target.Global.FunctionClass.New<JsArray>(CloneImpl), PropertyAttributes.DontEnum);
+            Target.Prototype.DefineOwnProperty("compact", Target.Global.FunctionClass.New<JsArray>(CompactImpl), PropertyAttributes.DontEnum);
+            Target.Prototype.DefineOwnProperty("first", Target.Global.FunctionClass.New<JsArray>(FirstImpl), PropertyAttributes.DontEnum);
+            Target.Prototype.DefineOwnProperty("last", Target.Global.FunctionClass.New<JsArray>(LastImpl), PropertyAttributes.DontEnum);
+            Target.Prototype.DefineOwnProperty("flatten", Target.Global.FunctionClass.New<JsArray>(FlattenImpl), PropertyAttributes.DontEnum);
+            Target.Prototype.DefineOwnProperty("indexOf", Target.Global.FunctionClass.New<JsArray>(IndexOfImpl), PropertyAttributes.DontEnum);
+            Target.Prototype.DefineOwnProperty("intersect", Target.Global.FunctionClass.New<JsArray>(IntersectImpl), PropertyAttributes.DontEnum);
+            
+            
+        }
+
+        public JsInstance IntersectImpl(JsArray target, JsInstance[] parameters)
+        {
+            if (parameters.Length == 0 || parameters[0].GetType() != typeof(JsArray)) return target;
+            var paramArray = (JsArray)parameters[0];
+            var shortArray = target.Length < paramArray.Length ? target : paramArray;
+            var longArray = target.Length > paramArray.Length ? target : paramArray;
+            var intersected = Target.Global.ArrayClass.New();
+            foreach (var item in shortArray)
+            {
+                foreach (var pair in findItem(longArray,item.Value))
+                {
+                    intersected[intersected.Length.ToString()] = pair.Value;
+                }
+            }
+            return intersected;
+        }
+
+        private IEnumerable<KeyValuePair<string, JsInstance>> 
+            findItem(IEnumerable<KeyValuePair<string, JsInstance>> target, JsInstance valueToFind)
+        {
+            return target.Where(item =>
+                                item.Value.GetType() == valueToFind.GetType() &&
+                                item.Value.Value.ToString() == valueToFind.Value.ToString());
+        }
+
+        public JsInstance IndexOfImpl(JsArray target, JsInstance[] parameters)
+        {
+            var result = Target.Global.NumberClass.New(-1);
+            if (parameters.Length == 0) return result;
+            var valueToFind = parameters[0];
+            foreach (var item in findItem(target,valueToFind))
+            {
+                return Target.Global.NumberClass.New(Convert.ToDouble(item.Key));
+            }
+            return result;
+        }
+
+        private void flattenTargetIntoResult(JsArray target, JsArray result)
+        {
+            for (var i = 0; i < target.Length; i++)
+            {
+                if (target[i.ToString()].GetType() != typeof(JsArray))
+                {
+                    result[result.Length.ToString()] = target[i.ToString()];
+                }
+                else
+                {
+                    flattenTargetIntoResult((JsArray)target[i.ToString()], result);
+                }
+            }
+        }
+
+        public JsInstance FlattenImpl(JsArray target, JsInstance[] parameters)
+        {
+            var result = Target.Global.ArrayClass.New();
+            flattenTargetIntoResult(target, result);
+            return result;
+        }
+
+        public JsInstance LastImpl(JsArray target, JsInstance[] parameters)
+        {
+            return target[(target.Length-1).ToString()];
+        }
+
+        public JsInstance FirstImpl(JsArray target, JsInstance[] parameters)
+        {
+            return target[0.ToString()];
+        }
+
+        public JsInstance CompactImpl(JsArray target, JsInstance[] parameters)
+        {
+            var result = Target.Global.ArrayClass.New();
+            for (var i = 0; i < target.Length; i++)
+            {
+                if (target[i.ToString()] != JsUndefined.Instance && target[i.ToString()] != JsNull.instance)
+                    result[result.Length.ToString()] = target[i.ToString()]; 
+            }
+            return result;
         }
 
         public JsInstance CollectImpl(JsObject target, JsInstance[] parameters)
